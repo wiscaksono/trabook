8b07e814b41c81dfe0e92654954f7756a0a9597a
wiscaksono <wwicaksono96@gmail.com>
2022-05-29 22:09:14 +0700

    trabook

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-1": "#FA7436",
+        "primary-2": "#222222",
+        "secondary-1": "#4086F4",
+        "secondary-2": "#FFB60A",
+        first: "#222222",
+        second: "#444444",
+        third: "#666666",
+      },
+    },
+  },
+  plugins: [],
+};
